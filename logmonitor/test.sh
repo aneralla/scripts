f8ed491f4c8eb9e3158a75b28eea8c267bc0c521
aneralla <aneralla@aetvn.com>
2016-05-05 18:05:05 -0400

    update to shell script

--- logmonitor/test.sh
+++ logmonitor/test.sh
@@ -1,34 +1,39 @@
-#!/bin/sh
 
-
+st.json
 
 awk -vDate=`date -d'now-30 minutes' +[%d/%b/%Y:%H:%M:%S` '$7 > Date {print Date, $0}' /var/log/httpd/dev-appletv.ott.aetnd.com_access.log | grep -v "200" > /tmp/log
-
-
 len=`cat /tmp/log | wc -l`
-
-
-
 a=1
 
+tim_temp=""
 
 while [ $a -le $len ]
 do
 
 >post.json
-
    x=`cat /tmp/log | awk '{FS=" "; print $12}' | head -$a| tail -1`
+
    url=$x
+
    resp=`cat /tmp/log | awk '{FS=" "; print $14}'| head -$a | tail -1`
+
    stack='appletv'
+
+   tim=`cat /tmp/log | awk '{FS=" "; print $1}' | sed 's/\[//g' | tail -1`
+
    a=`expr $a + 1`
 
    echo "{" >> post.json
    echo "\"url\":\"$url\"," >> post.json
    echo "\"http_response_code\":\"$resp\"," >> post.json
-   echo "\"stack\":\"$stack\"" >> post.json
+   echo "\"stack\":\"$stack\"," >> post.json
+   echo "\"time\":\"$tim\"" >> post.json
    echo "}" >> post.json
 
-curl -v -X POST "http://ec2-54-86-253-113.compute-1.amazonaws.com/write.php" -d @post.json
+if [ "$tim" != "$tim_temp" ]
+   then
+      curl -v -X POST "http://ec2-54-86-253-113.compute-1.amazonaws.com/write.php" -d @post.json
+      tim_temp=$tim
+fi
 
 done
